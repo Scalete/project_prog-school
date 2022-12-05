2fb009b07ebe4971ff434f24f2175f9310427e05
Maxim <maximpalaguta@gmail.com>
2022-12-05 03:11:40 +0200

    added header

--- src/js/header.js
+++ src/js/header.js
@@ -1,50 +1,48 @@
-// document.addEventListener('DOMContentLoaded', function() { 
-//     const body = document.querySelector('body')
-//     hamburger = body.querySelector('.hamburger'),
-//     nav = body.querySelector('nav'),
-//     closeElem = body.querySelector('.nav-close'),
-//     overlay = body.querySelector('.header-overlay');
+document.addEventListener('DOMContentLoaded', function() { 
+    const body = document.querySelector('body')
+    header = body.querySelector('header'),
+    hamburger = header.querySelector('.hamburger'),
+    nav = header.querySelector('header nav'),
+    closeElem = header.querySelector('.close-mobile'),
+    overlay = body.querySelector('.overlay');
 
-//     let resizeTime;
+    let resizeTime;
 
-//     //open nav
+    //open nav
 
-//     hamburger.addEventListener('click', () => {
-//         nav.classList.add('active');
-//         overlay.style.display = 'block';
-//         body.classList.add('overflow-hide');
-//     });
+    hamburger.addEventListener('click', () => {
+        overlay.classList.add('active');
+        nav.classList.add('active');
+        body.classList.add('overflow-hide');
+    });
 
-//     //close nav
+    //close nav
 
-//     function closeNav() {
-//         nav.classList.remove('active');
-//         body.classList.remove('overflow-hide');
-//         overlay.style.display = 'none';
-//     }
+    function closeNav() {
+        nav.classList.remove('active');
+        body.classList.remove('overflow-hide');
+        overlay.classList.remove('active');
+    }
 
-//     //close on resize
+    //close on resize
     
-//     window.addEventListener('resize', () => {
-//         clearTimeout(resizeTime);
-//         resizeTime = setTimeout(closeNav, 100);
-//     });
+    window.addEventListener('resize', () => {
+        clearTimeout(resizeTime);
+        resizeTime = setTimeout(closeNav, 100);
+    });
 
-//     // close on click
-//     closeElem.addEventListener('click', () => {
-//         closeNav(closeElem);
-//     });
-    
-//     // close on overlay
-//     overlay.addEventListener('click', () => {
-//         closeNav(closeElem);
-//     });
+    // close on click, overlay
+    [closeElem, overlay].forEach(item => {
+        item.addEventListener('click', () => {
+            closeNav(closeElem);
+        })
+    });
 
-//     //close on escape
+    //close on escape
 
-//     document.addEventListener('keyup', function(e) {
-//         if (e.code === 'Escape' && nav.classList.contains("active")) {
-//             closeNav(closeElem);
-//         }
-//     });
-// });
+    document.addEventListener('keyup', function(e) {
+        if (e.code === 'Escape' && nav.classList.contains("active")) {
+            closeNav(closeElem);
+        }
+    });
+});
